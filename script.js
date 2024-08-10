d35ddf8f9463930ee19befc888d6c20c9a90c23a
pryntl <pryntl2007@gmail.com>
2024-08-10 15:54:35 +0330

    feat [java script]
    added a function which counts the number of vowels of the word.

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-
+function countVowels(word) {
+  let output = 0;
+  const vowels = "aeiouAEIOU";
+  output = word.split("").filter((vwl) => vowels.includes(vwl)).length;
+  return output;
+}
+console.log(countVowels("ipoheghi"));
