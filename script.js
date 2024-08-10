c33f7251b60ce0041a9a5deb6758dca9671f0dda
paryan <pryntl2007@gmail.com>
2024-08-10 16:01:08 +0330

    Update [java script] : comment
    added comments to explain the code.

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
+//added function base code.
 function countVowels(word) {
+  //made a variable to show the last result.
   let output = 0;
+  //defining the vowels.
   const vowels = "aeiouAEIOU";
+  //turning the word from string to array(using split method.) so we can use filter method which is onle for arrays. then we check how many vowles it includes (using length method).
   output = word.split("").filter((vwl) => vowels.includes(vwl)).length;
+  //returning the main var at the end.
   return output;
 }
+//testing.
 console.log(countVowels("ipoheghi"));
